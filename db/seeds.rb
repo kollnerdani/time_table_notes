2faf1737b02db366563680eedccb6bb3403f00d8
Daniel Kollner <dankollner@gmail.com>
2024-11-09 19:55:16 +0100

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.first_or_create(email: "testemail@example.com", password: Devise.friendly_token.first(6))
+
+20.times do |i|
+  WorkingTime.create(date: Date.today + ((1..30)&.to_a&.sample).days, start_time: %w[07:00 08:00 09:00 10:00].sample, duration: (1..8).to_a.sample, user: User.last, title: "Title #{i}", description: "Description #{i}")
+end
